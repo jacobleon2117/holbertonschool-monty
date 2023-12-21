ee7bf8b4b5878b72fde992ef0b17ebe223bdf930
jacobleon2117 <jacobleon2117@gmail.com>
2023-12-21 00:38:16 -0600

    nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing, place holder
+ * @stack: the stack
+ * @line_number: number of line being read
+ **/
+
+void nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) unsigned int line_number)
+{
+
+}
