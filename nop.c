0aef12f40451530e8110ce120ab15e4d2a9b1d74
jacobleon2117 <jacobleon2117@gmail.com>
2023-12-21 00:42:23 -0600

    fixing whitespace

--- nop.c
+++ nop.c
@@ -6,7 +6,7 @@
  * @line_number: number of line being read
  **/
 
-void nop(__attribute__((unused)) stack_t **stack, __attribute__((unused)) 
+void nop(__attribute__((unused)) stack_t **stack, __attribute__((unused))
 unsigned int line_number)
 {
 
